86e120578befe680d00d802210f493ca3f3e7703
Franz-Martin Herstell <Franz.Herstell@gmx.de>
2025-12-04 10:03:23 +0100

    Initial commit

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,11 +1,8 @@
 -- Spring Boot Basic Tag 9 - Test Daten
 
-INSERT INTO persons (id, firstname, lastname, email, created_at) VALUES 
-(1, 'Max', 'Mustermann', 'max.mustermann@example.com', CURRENT_TIMESTAMP),
-(2, 'Erika', 'Musterfrau', 'erika.musterfrau@example.com', CURRENT_TIMESTAMP),
-(3, 'Tom', 'Miller', 'tom.miller@example.com', CURRENT_TIMESTAMP),
-(4, 'Anna', 'Schmidt', 'anna.schmidt@example.com', CURRENT_TIMESTAMP),
-(5, 'Peter', 'Müller', 'peter.mueller@example.com', CURRENT_TIMESTAMP);
-
--- Sequenz für neue Einträge anpassen
-ALTER TABLE persons ALTER COLUMN id RESTART WITH 6;
+INSERT INTO persons (firstname, lastname, email, created_at) VALUES
+('Max', 'Mustermann', 'max.mustermann@example.com', CURRENT_TIMESTAMP),
+('Erika', 'Musterfrau', 'erika.musterfrau@example.com', CURRENT_TIMESTAMP),
+('Tom', 'Miller', 'tom.miller@example.com', CURRENT_TIMESTAMP),
+('Anna', 'Schmidt', 'anna.schmidt@example.com', CURRENT_TIMESTAMP),
+('Peter', 'Müller', 'peter.mueller@example.com', CURRENT_TIMESTAMP);
